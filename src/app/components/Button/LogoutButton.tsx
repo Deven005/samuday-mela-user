81948bd9d89d2a840d2e889d1d0294ac0241ecc6
devenChavda-dev <deven.chavda@preeshe.com>
2025-05-12 11:40:52 +0530

    Logout button client component...

--- src/app/components/Button/LogoutButton.tsx
+++ src/app/components/Button/LogoutButton.tsx
@@ -1,17 +1,22 @@
 // app/components/LogoutButton.tsx
-"use client";
-import { useUserStore } from "@/app/stores/user/userStore";
-import { useRouter } from "next/navigation";
+'use client';
+import { useUserStore } from '@/app/stores/user/userStore';
+import { useRouter } from 'next/navigation';
 
 export default function LogoutButton() {
   const router = useRouter();
   const logout = useUserStore((state) => state.logoutUser);
 
   const logoutClick = async () => {
-    await logout();
-    localStorage.clear();
-    router.push("/sign-in");
-    router.refresh();
+    try {
+      await logout();
+    } catch (error) {
+      console.log('logoutClick: ', error);
+    } finally {
+      router.push('/sign-in');
+      router.refresh();
+      localStorage.clear();
+    }
   };
 
   return (
