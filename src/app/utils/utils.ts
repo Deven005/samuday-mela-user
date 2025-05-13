179ae427398d8d4842bbc7f9d8363564fc8925cf
devenChavda-dev <deven.chavda@preeshe.com>
2025-05-13 15:22:15 +0530

    added use-full functions getDistanceFromLatLonInKm and formatLocalDate

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -85,3 +85,26 @@ export async function getNotifications() {
     return [];
   }
 }
+
+export function getDistanceFromLatLonInKm(lat1: number, lon1: number, lat2: number, lon2: number) {
+  const R = 6371; // Radius of the earth in km
+  const dLat = ((lat2 - lat1) * Math.PI) / 180;
+  const dLon = ((lon2 - lon1) * Math.PI) / 180;
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos((lat1 * Math.PI) / 180) *
+      Math.cos((lat2 * Math.PI) / 180) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c; // Distance in km
+}
+
+export function formatLocalDate(date: Date, timeZone: string) {
+  return new Intl.DateTimeFormat('en-US', {
+    timeZone,
+    dateStyle: 'medium',
+    timeStyle: 'short',
+  }).format(date);
+}
