e38f29a23d2a6e44a85c0deefb495c6468b7e1fa
devenChavda-dev <deven.chavda@preeshe.com>
2025-05-22 17:45:26 +0530

    added reload user, auth.languageCode = 'hi' and setPersistence(browserLocalPersistence)

--- src/app/components/Footer/FooterClient.tsx
+++ src/app/components/Footer/FooterClient.tsx
@@ -1,8 +1,24 @@
 'use client';
+import { auth } from '@/app/config/firebase.config';
 import useFCM from '@/app/hooks/useFCM';
+import { useUserStore } from '@/app/stores/user/userStore';
+import { browserLocalPersistence } from 'firebase/auth';
+import { useEffect } from 'react';
+import { useShallow } from 'zustand/shallow';
 
 const FooterClient = () => {
+  const { reloadUser } = useUserStore(useShallow((state) => state));
+
   useFCM();
+  reloadUser().then(
+    (res) => {},
+    (err) => {},
+  );
+
+  useEffect(() => {
+    auth.languageCode = 'hi';
+    auth.setPersistence(browserLocalPersistence).then();
+  }, []);
 
   return <></>;
 };
