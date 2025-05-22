eca177e35fc5c83c69817d8f11d3743082a12840
devenChavda-dev <deven.chavda@preeshe.com>
2025-05-22 17:23:35 +0530

    added className, ref, accept param

--- src/app/components/Input/InputField.tsx
+++ src/app/components/Input/InputField.tsx
@@ -1,14 +1,17 @@
 'use client';
-import React from 'react';
+import React, { RefObject } from 'react';
 
 interface InputFieldProps {
   id: string;
   label: string;
-  type?: 'text' | 'email' | 'password' | 'number';
+  type?: 'text' | 'email' | 'password' | 'number' | 'file';
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   required?: boolean;
   placeholder?: string;
+  ref?: RefObject<HTMLInputElement | null>;
+  accept?: string | undefined;
+  className?: string | undefined;
 }
 
 const InputField: React.FC<InputFieldProps> = ({
@@ -19,6 +22,9 @@ const InputField: React.FC<InputFieldProps> = ({
   onChange,
   required = false,
   placeholder,
+  ref,
+  accept,
+  className,
 }) => {
   return (
     <div className="form-control mb-4 transition duration-200 hover:scale-105 text-base-content">
@@ -30,11 +36,13 @@ const InputField: React.FC<InputFieldProps> = ({
         id={id}
         name={id}
         type={type}
-        className="input input-md w-full bg-base-100 text-base-content border border-base-300 focus:ring-indigo-500 focus:border-indigo-500 rounded-md p-2 transition duration-200"
+        className={`input input-md w-full bg-base-100 text-base-content border border-base-300 focus:ring-indigo-500 focus:border-indigo-500 rounded-md p-2 transition duration-200 ${className}`}
         value={value}
         onChange={onChange}
         required={required}
         placeholder={placeholder || label}
+        accept={accept}
+        ref={ref}
       />
     </div>
   );
