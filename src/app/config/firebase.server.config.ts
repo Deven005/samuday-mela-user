b7a58a5fcb0cb40cb36fc4369fd36d198ed010fd
devenChavda-dev <deven.chavda@preeshe.com>
2025-05-27 23:59:21 +0530

    added serverStorage ref / var

--- src/app/config/firebase.server.config.ts
+++ src/app/config/firebase.server.config.ts
@@ -4,6 +4,7 @@ import { getAppCheck } from 'firebase-admin/app-check';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getMessaging } from 'firebase-admin/messaging';
+import { getStorage } from 'firebase-admin/storage';
 
 // Set the config options
 // Initialize the firebase admin app
@@ -17,11 +18,13 @@ const firebaseServerApp =
           clientEmail: process.env.CLIENT_EMAIL,
         } as ServiceAccount),
         projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
       });
 
 const serverAppCheck = getAppCheck(firebaseServerApp);
 const serverAuth = getAuth(firebaseServerApp);
 const serverFirestore = getFirestore(firebaseServerApp);
 const serverMessaging = getMessaging(firebaseServerApp);
+const serverStorage = getStorage(firebaseServerApp);
 
-export { serverAppCheck, serverAuth, serverFirestore, serverMessaging };
+export { serverAppCheck, serverAuth, serverFirestore, serverMessaging, serverStorage };
