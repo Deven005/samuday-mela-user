3d2d2de69078c374b97c815bf74e0b4bbbce5e20
devenChavda-dev <deven.chavda@preeshe.com>
2025-05-27 23:59:54 +0530

    added listenToUser and moved reloadUser inside useEffect

--- src/app/components/Footer/FooterClient.tsx
+++ src/app/components/Footer/FooterClient.tsx
@@ -2,22 +2,21 @@
 import { auth } from '@/app/config/firebase.config';
 import useFCM from '@/app/hooks/useFCM';
 import { useUserStore } from '@/app/stores/user/userStore';
-import { browserLocalPersistence } from 'firebase/auth';
 import { useEffect } from 'react';
-import { ToastContainer } from 'react-toastify';
 import { useShallow } from 'zustand/shallow';
 
 const FooterClient = () => {
-  const { reloadUser } = useUserStore(useShallow((state) => state));
+  const { reloadUser, listenToUser } = useUserStore(useShallow((state) => state));
 
   useFCM();
-  reloadUser().then(
-    (res) => {},
-    (err) => {},
-  );
 
   useEffect(() => {
     auth.languageCode = 'hi';
+    reloadUser().then(
+      (res) => {},
+      (err) => {},
+    );
+    listenToUser();
     // auth.setPersistence(browserLocalPersistence).then();
   }, []);
 
