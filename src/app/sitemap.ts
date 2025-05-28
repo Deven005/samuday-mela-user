c6342b3aa8f2ba7995c12cb1ba07c3a43c080f1e
devenChavda-dev <deven.chavda@preeshe.com>
2025-05-29 02:24:42 +0530

    added sign-in, sign-up, faq, posts, posts/add, pages for sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,6 +9,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: 'https://samuday-mela-user.vercel.app/sign-in',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://samuday-mela-user.vercel.app/sign-up',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
       url: 'https://samuday-mela-user.vercel.app/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
@@ -21,7 +33,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.5,
     },
     {
+      url: 'https://samuday-mela-user.vercel.app/faq',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
       url: 'https://samuday-mela-user.vercel.app/legal',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://samuday-mela-user.vercel.app/posts',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://samuday-mela-user.vercel.app/posts/add',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.5,
