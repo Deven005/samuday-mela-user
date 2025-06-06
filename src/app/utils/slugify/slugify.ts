d9f65a500979b6cb3d48010779bcf206e0373ab4
devenChavda-dev <deven.chavda@preeshe.com>
2025-06-07 01:36:55 +0530

    added method for generateSlug unique

--- src/app/utils/slugify/slugify.ts
+++ src/app/utils/slugify/slugify.ts
@@ -1,10 +1,11 @@
 // util/slugify.ts
-export const generateSlug = (text: string) => {
-  return text
+export const generateSlug = (text: string, uniqueId: string) => {
+  let slug = text
     .toLowerCase()
     .trim()
     .replace(/[^a-z0-9]+/g, '-') // Convert spaces/special chars to "-"
     .replace(/^-+|-+$/g, '') // Remove leading/trailing dashes
     .replace(/-{2,}/g, '-') // Remove duplicate dashes
     .slice(0, 50);
+  return `${slug}-${uniqueId.slice(0, 4)}-${uniqueId.slice(-4)}`;
 };
