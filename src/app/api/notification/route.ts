c7ecd315bef11c13f1b93467acb08291050012a9
devenChavda-dev <mymartplac@gmail.com>
2025-11-22 01:56:07 +0530

    Updated...

--- src/app/api/notification/route.ts
+++ src/app/api/notification/route.ts
@@ -1,5 +1,6 @@
 // /api/notification/route.ts
 import { serverMessaging } from '@/app/config/firebase.server.config';
+import { AndroidConfig, WebpushConfig } from 'firebase-admin/messaging';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const runtime = 'nodejs'; // Ensure this runs in Node.js
@@ -8,50 +9,55 @@ export async function POST(req: NextRequest) {
   try {
     const reqBody = await req.json();
 
-    const { topic, title, body, image, imageUrl, icon, logo } = reqBody;
+    const { topic, topics, title, body, image, imageUrl, icon, logo } = reqBody;
 
-    if (!topic) {
+    if (!topic && (!topics || (topics as Array<string>).length == 0)) {
       return NextResponse.json({ error: 'No topic provided' }, { status: 400 });
     }
 
-    await serverMessaging.send({
-      topic: topic,
-      android: {
-        notification: {
-          priority: 'high',
-          title: title ?? 'notification title',
-          body: body ?? 'notification body',
-          imageUrl: image ?? imageUrl,
-          icon:
-            icon ??
-            'https://firebasestorage.googleapis.com/v0/b/samudaymela.appspot.com/o/public%2FAppLogo.png?alt=media&token=c2d303b5-c27f-4bde-a8c4-5bb434c30237',
-        },
+    const android: AndroidConfig = {
+      notification: {
+        priority: 'high',
+        title: title ?? 'notification title',
+        body: body ?? 'notification body',
+        imageUrl: image ?? imageUrl,
+        icon:
+          icon ??
+          'https://firebasestorage.googleapis.com/v0/b/samudaymela.appspot.com/o/public%2FAppLogo.png?alt=media&token=c2d303b5-c27f-4bde-a8c4-5bb434c30237',
       },
-      webpush: {
-        headers: {
-          Urgency: 'high',
-          priority: 'high',
-        },
-        data: {
-          urgent: 'true',
-          priority: 'high',
-          title: title ?? 'notification title',
-          body: body ?? 'notification body',
-          imageUrl: image ?? imageUrl ?? '',
-          logo:
-            logo ??
-            'https://firebasestorage.googleapis.com/v0/b/samudaymela.appspot.com/o/public%2FAppLogo.png?alt=media&token=c2d303b5-c27f-4bde-a8c4-5bb434c30237',
-        },
-        notification: {
-          title,
-          body,
-          icon:
-            icon ??
-            'https://firebasestorage.googleapis.com/v0/b/samudaymela.appspot.com/o/public%2FAppLogo.png?alt=media&token=c2d303b5-c27f-4bde-a8c4-5bb434c30237',
-          image: image ?? imageUrl,
-        },
+    };
+    const webpush: WebpushConfig = {
+      headers: {
+        Urgency: 'high',
+        priority: 'high',
       },
-    });
+      data: {
+        urgent: 'true',
+        priority: 'high',
+        title: title ?? 'notification title',
+        body: body ?? 'notification body',
+        imageUrl: image ?? imageUrl ?? '',
+        logo:
+          logo ??
+          'https://firebasestorage.googleapis.com/v0/b/samudaymela.appspot.com/o/public%2FAppLogo.png?alt=media&token=c2d303b5-c27f-4bde-a8c4-5bb434c30237',
+      },
+      notification: {
+        title,
+        body,
+        icon:
+          icon ??
+          'https://firebasestorage.googleapis.com/v0/b/samudaymela.appspot.com/o/public%2FAppLogo.png?alt=media&token=c2d303b5-c27f-4bde-a8c4-5bb434c30237',
+        image: image ?? imageUrl,
+      },
+    };
+
+    await serverMessaging.sendEach(
+      ((topics ?? [topic]) as Array<string>).map((topic) => ({
+        topic,
+        android,
+        webpush,
+      })),
+    );
     console.log('Notification is sent!');
     return NextResponse.json({ message: 'Notification is sent!' }, { status: 200 });
   } catch (error) {
